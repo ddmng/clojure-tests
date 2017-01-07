78e03e7dd8fa78fe4e4d30b826f726f706196307
Daniele Di Minica <d.diminica@resi.it>
2017-01-07 19:03:45 +0100

    excercises 1 and 2

---
+++ README.md
@@ -0,0 +1,41 @@
+# test1
+
+Playing a bit with clojure
+
+Following tutorial and articles to learn clj.
+List of tutorials/articles:
+1. http://www.braveclojure.com/concurrency/
+
+
+## Installation
+
+Download from http://example.com/FIXME.
+
+## Usage
+
+FIXME: explanation
+
+    $ java -jar test1-0.1.0-standalone.jar [args]
+
+## Options
+
+FIXME: listing of options this app accepts.
+
+## Examples
+
+...
+
+### Bugs
+
+...
+
+### Any Other Sections
+### That You Think
+### Might be Useful
+
+## License
+
+Copyright © 2017 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ src/test1/core.clj
@@ -0,0 +1,101 @@
+(ns test1.core
+  (:gen-class))
+
+(declare search-string)
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  ; (log "Hello, World!" 1 2 3)
+  ;(future-test)
+  ;(future-test)
+  ;(wisdom-test)
+  (def engines {:bing "http://www.bing.com/?q=" :google "http://www.google.com/?#q="})
+  (search-string "Daniele" engines)
+)
+
+(defn search-string [arg engines]
+    ( doseq [engine engines]
+      (let [result (future (slurp (str (val engine) arg)))]
+        (println "--> " (key engine) (count @result)) (spit (str (key engine) "-" arg ".html") @result)
+    )
+  ))
+
+(defn future-test []
+  (let [a 5000 result (future (println "Going to sleep for " a "msecs") (Thread/sleep a) (println "this prints once")
+                       (+ 1 1))]
+    (println "@: " @result)))
+
+(def yak-butter-international
+  {:store "Yak Butter International"
+    :price 90
+    :smoothness 90})
+(def butter-than-nothing
+  {:store "Butter Than Nothing"
+   :price 150
+   :smoothness 83})
+;; This is the butter that meets our requirements
+(def baby-got-yak
+  {:store "Baby Got Yak"
+   :price 94
+   :smoothness 99})
+
+(defn mock-api-call
+  [result]
+  (println "Fake api call")
+  (Thread/sleep 1000)
+  result)
+
+(defn satisfactory?
+  "If the butter meets our criteria, return the butter, else return false"
+  [butter]
+  (println "Trying if " butter " satisfies us")
+  (and (<= (:price butter) 100)
+       (>= (:smoothness butter) 97)
+       butter))
+
+(defn promises-and-futures []
+  (time
+    (let [butter-promise (promise)]
+      (doseq [butter [yak-butter-international butter-than-nothing baby-got-yak]]
+        (future (if-let [satisfactory-butter (satisfactory? (mock-api-call butter))]
+            (deliver butter-promise satisfactory-butter))))
+      (println "And the winner is:" (deref butter-promise 1015 "timeout!")))))
+
+(defn wisdom-test []
+    (let [ferengi-wisdom-promise (promise)]
+      (future (println "Here's some Ferengi wisdom:" @ferengi-wisdom-promise))
+      (Thread/sleep 100)
+      (deliver ferengi-wisdom-promise "Whisper your way to success.")))
+
+(defmacro wait
+  "Sleep `timeout` seconds before evaluating body"
+  [timeout & body]
+  `(do (Thread/sleep ~timeout) ~@body))
+
+;  (let [saying3 (promise)]
+;    (future (deliver saying3 (wait 100 "Cheerio!")))
+;    @(let [saying2 (promise)]
+;       (future (deliver saying2 (wait 400 "Pip pip!")))
+;          @(let [saying1 (promise)]
+;          (future (deliver saying1 (wait 200 "'Ello, gov'na!")))
+;          (println @saying1)
+;          saying1)
+;       (println @saying2)
+;       saying2)
+;    (println @saying3)
+;    saying3)
+
+(defmacro enqueue
+    ([q concurrent-promise-name concurrent serialized]
+     `(let [~concurrent-promise-name (promise)]
+      (future (deliver ~concurrent-promise-name ~concurrent))
+      (deref ~q)
+      ~serialized
+      ~concurrent-promise-name))
+    ([concurrent-promise-name concurrent serialized]
+   `(enqueue (future) ~concurrent-promise-name ~concurrent ~serialized)))
+
+;(-> (enqueue saying (wait 200 "'Ello, gov'na!") (println @saying))
+;    (enqueue saying (wait 400 "Pip pip!") (println @saying))
+;    (enqueue saying (wait 100 "Cheerio!") (println @saying)))
